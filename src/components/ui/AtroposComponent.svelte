5ca62b36feb59077615f95ef290510b4fd1daedc
SensibleBuilder <JERRY@SENSIBLEBUILDER.COM>
2025-08-16 19:08:11 +0200

    Make gallery dynamic and clickable with Atropos; add nav arrows

---
+++ src/components/ui/AtroposComponent.svelte
@@ -0,0 +1,27 @@
+<script>
+    import { onMount } from 'svelte';
+
+    let defined = false;
+
+    onMount(async () => {
+        if (!defined && typeof window !== 'undefined') {
+            const AtroposComponent = (await import('atropos/element')).default;
+            if (!customElements.get('atropos-component')) {
+                customElements.define('atropos-component', AtroposComponent);
+            }
+            defined = true;
+        }
+    });
+
+</script>
+
+<atropos-component
+    class="atropos"
+    active-offset="20"
+    shadow-scale="0"
+    rotate-y-invert="true"
+    rotate-x-max="0"
+    rotate-y-max="1"
+>
+    <slot />
+</atropos-component>
